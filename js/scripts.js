c92cea1485f4fc8125c8966963a9bddd42cdae64
Anna Becker and Tyler Suderman <tcsuder@gmail.com>
2016-01-19 16:34:35 -0800

    refactor specs and scripts and fix html text problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 var triangle = function(side1, side2, side3) {
-  if ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)) {
-    return 'not a triangle';
+  if (isNotTriangle(side1, side2, side3)) {
+    return 'isNotTriangle';
   }
-  else if
-    (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+  else {
+    return triangleType(side1, side2, side3);
+  }
+};
+
+var isNotTriangle = function(side1, side2, side3) {
+  return ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1))
+};
+
+var triangleType = function(side1, side2, side3) {
+  if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
     return 'scalene';
   }
   else if ((side1 === side2 || side1 === side3 || side2 === side3) && ((side1 + side2 + side3) / 3 !== side1)) {
@@ -14,21 +23,21 @@ var triangle = function(side1, side2, side3) {
   }
 };
 
-$(document).ready(function(){
 
+$(document).ready(function(){
   $("form#triangle").submit(function(event) {
     var side1 = parseInt($("input#side1").val());
     var side2 = parseInt($("input#side2").val());
     var side3 = parseInt($("input#side3").val());
 
-      if (triangle(side1,side2,side3) === "not a triangle") {
+      if (triangle(side1,side2,side3) === 'isNotTriangle') {
         $("#notTriangle").show();
       }
       else if
-        (triangle(side1,side2,side3) === "scalene") {
+        (triangle(side1,side2,side3) === 'scalene') {
         $("#scalene").show();
       }
-      else if (triangle(side1,side2,side3) === "isosceles") {
+      else if (triangle(side1,side2,side3) === 'isosceles') {
         $("#isosceles").show();
       }
       else {
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,31 @@
-describe('triangle', function() {
-  it("is false when the sum of any two sides is less than or equal to the third.", function() {
-    expect(triangle(1,2,3)).to.equal('Not a triangle');
+describe('isNotTriangle', function() {
+  it("is false when sides form valid triangle", function() {
+    expect(isNotTriangle(4,8,10)).to.equal(false);
+  });
+  it("is true when the sum of any two sides is less than or equal to the third.", function() {
+    expect(isNotTriangle(1,2,3)).to.equal(true);
+  });
+});
+
+describe('triangleType', function() {
+  it("return scalene when no sides are equal", function() {
+    expect(triangleType(4,8,10)).to.equal('scalene');
   });
 
+  it("return isosceles when exactly 2 sides are equal", function() {
+    expect(triangleType(3,3,2)).to.equal('isosceles');
+  });
+
+  it("return equilateral when all sides are equal", function() {
+    expect(triangleType(3,3,3)).to.equal('equilateral');
+  });
+});
+
+describe('triangle', function() {
   it("is true when no sides are equal", function() {
     expect(triangle(4,8,10)).to.equal('scalene');
   });
-
-  it("is true when exactly 2 sides are equal", function() {
-    expect(triangle(3,3,2)).to.equal('isosceles');
-  });
-
-  it("is true when all sides are equal", function() {
-    expect(triangle(3,3,3)).to.equal('equilateral');
+  it("is false when the sum of any two sides is less than or equal to the third.", function() {
+    expect(triangle(1,2,3)).to.equal('not a triangle');
   });
 });
